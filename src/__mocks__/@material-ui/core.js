e282ae4912f5d675e2a1079f25d3d52f0388ecff
Bartosz <rak.bartosz98@gmail.com>
2019-01-24 17:09:26 +0100

    feature: Test mocks improvements.

---
+++ src/__mocks__/@material-ui/core.js
@@ -0,0 +1,10 @@
+import { makeGetProxy } from '../../utils/proxies'
+
+// Custom mocks
+const definedMocks = {
+  createMuiTheme: jest.fn(),
+  withStyles: jest.fn(() => jest.fn(component => component)),
+}
+
+// Mock any not defined components automatically: Component -> ComponentMock
+module.exports = makeGetProxy(definedMocks, (target, name) => `${name}Mock`)
