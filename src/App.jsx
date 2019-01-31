668fcc7c7e046b5ab1c2f29237fad5a881bb0c9e
Bartosz Rak <rak.bartosz98@gmail.com>
2019-01-31 02:09:18 +0100

    feature: React-toastify has been initialized #RAB-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ToastContainer } from 'react-toastify'
 import { BrowserRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import firebase from './services/firebase'
@@ -34,7 +35,10 @@ export class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Layout />
+        <React.Fragment>
+          <Layout />
+          <ToastContainer />
+        </React.Fragment>
       </BrowserRouter>
     )
   }
